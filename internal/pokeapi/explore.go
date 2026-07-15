c981fe67936c61e0b7704b8285d65dfa53b111ba
AlRowne <aroth@mailbox.org>
2026-07-15 07:45:35 +0200

    add explore command

---
+++ internal/pokeapi/explore.go
@@ -0,0 +1,50 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ExploreResponse struct {
+	PokemonEncounters []PokemonEncounter `json:"pokemon_encounters"`
+}
+
+type PokemonEncounter struct {
+	Pokemon PokemonInfo `json:"pokemon"`
+}
+
+type PokemonInfo struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+func GetPokemonEncounters(locationName string) (ExploreResponse, error) {
+	url := baseURL + locationName + "/"
+
+	cacheRes, ok := cache.Get(url)
+	if ok {
+		var exploreResponse ExploreResponse
+		if err := json.Unmarshal(cacheRes, &exploreResponse); err != nil {
+			return ExploreResponse{}, err
+		}
+		return exploreResponse, nil
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return ExploreResponse{}, err
+	}
+	defer res.Body.Close()
+
+	var exploreResponse ExploreResponse
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ExploreResponse{}, err
+	}
+	if err := json.Unmarshal(data, &exploreResponse); err != nil {
+		return ExploreResponse{}, err
+	}
+	cache.Add(url, data)
+
+	return exploreResponse, nil
+}
--- main.go
+++ main.go
@@ -13,13 +13,13 @@ type config struct {
 	Previous string
 }
 
-func commandExit(cfg *config) error {
+func commandExit(cfg *config, s string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, s string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
@@ -29,7 +29,7 @@ func commandHelp(cfg *config) error {
 	}
 	return nil
 }
-func commandMap(cfg *config) error {
+func commandMap(cfg *config, s string) error {
 	locationAreas, err := pokeapi.GetLocationAreas(cfg.Next)
 	if err != nil {
 		return err
@@ -43,7 +43,7 @@ func commandMap(cfg *config) error {
 	return nil
 }
 
-func commandMapb(cfg *config) error {
+func commandMapb(cfg *config, s string) error {
 	if cfg.Previous == "" {
 		fmt.Println("you're on the first page")
 		return nil
@@ -62,6 +62,21 @@ func commandMapb(cfg *config) error {
 	return nil
 }
 
+func commandExplore(cfg *config, s string) error {
+	fmt.Printf("Exploring %s...\n", s)
+	response, err := pokeapi.GetPokemonEncounters(s)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Found Pokemon:")
+
+	for _, encounter := range response.PokemonEncounters {
+		fmt.Printf(" - %s\n", encounter.Pokemon.Name)
+	}
+	return nil
+}
+
 func stringOrEmpty(s *string) string {
 	if s == nil {
 		return ""
@@ -72,7 +87,7 @@ func stringOrEmpty(s *string) string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -97,6 +112,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays the previous 20 Location-Areas",
 			callback:    commandMapb,
 		},
+		"explore": {
+			name:        "explore",
+			description: "Shows all Pokemon in the provided area",
+			callback:    commandExplore,
+		},
 	}
 	return cliCommands
 }
@@ -118,13 +138,21 @@ func main() {
 			continue
 		}
 		command := words[0]
+		explArea := ""
+		if command == "explore" && len(words) < 2 {
+			fmt.Println("Please provide an area to explore. (Use the map/mapb command)")
+			continue
+		} else if command == "explore" && len(words) >= 2 {
+			explArea = words[1]
+		}
+
 		commands := getCommands()
 		val, ok := commands[command]
 		if !ok {
 			fmt.Println("Unknown command")
 			continue
 		}
-		if err := val.callback(&cfg); err != nil {
+		if err := val.callback(&cfg, explArea); err != nil {
 			fmt.Println(err)
 		}
 	}
